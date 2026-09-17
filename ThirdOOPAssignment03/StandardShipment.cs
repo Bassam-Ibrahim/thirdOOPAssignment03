7b182cc1fb2b7bda0db83cb39f7e47b509cee73e
Bassam Ibrahim Abu El-Saud <bassamb672005@gmail.com>
2026-09-18 01:23:06 +0300

    StandardShipment Class

---
+++ ThirdOOPAssignment03/StandardShipment.cs
@@ -0,0 +1,30 @@
+﻿using ThirdOOPAssignment;
+
+namespace ThirdOOPAssignment
+{
+    public class StandardShipment : Shipment
+    {
+        public StandardShipment(
+            string trackingCode,
+            string description,
+            decimal weight,
+            decimal deliveryFee,
+            DeliveryAddress destination)
+            : base(
+                trackingCode,
+                description,
+                weight,
+                deliveryFee,
+                destination)
+        {
+        }
+
+        public override void PrintShipment()
+        {
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine("Standard Shipment");
+
+            base.PrintShipment();
+        }
+    }
+}
