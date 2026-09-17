bf674d1d2ef415ed1b15bbf05bf35bf8556a4508
Bassam Ibrahim Abu El-Saud <bassamb672005@gmail.com>
2026-09-18 01:21:58 +0300

    Shipment Class

---
+++ ThirdOOPAssignment03/Shipment.cs
@@ -0,0 +1,121 @@
+﻿using ThirdOOPAssignment;
+
+namespace ThirdOOPAssignment
+{
+    public class Shipment
+    {
+        private string trackingCode;
+        private string description;
+        private decimal weight;
+        private decimal deliveryFee;
+        private DeliveryAddress destination;
+
+        public string TrackingCode
+        {
+            get { return trackingCode; }
+
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                    trackingCode = value;
+            }
+        }
+
+        public string Description
+        {
+            get { return description; }
+
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                    description = value;
+            }
+        }
+
+        public decimal Weight
+        {
+            get { return weight; }
+
+            set
+            {
+                if (value > 0)
+                    weight = value;
+            }
+        }
+
+        public decimal DeliveryFee
+        {
+            get { return deliveryFee; }
+
+            set
+            {
+                if (value >= 0)
+                    deliveryFee = value;
+            }
+        }
+
+        public DeliveryAddress Destination
+        {
+            get { return destination; }
+            set { destination = value; }
+        }
+
+        public Shipment(string trackingCode)
+        {
+            TrackingCode = trackingCode;
+            description = "Unknown";
+            weight = 1;
+            deliveryFee = 0;
+            destination = new DeliveryAddress();
+        }
+
+        public Shipment(
+            string trackingCode,
+            string description,
+            decimal weight,
+            decimal deliveryFee,
+            DeliveryAddress destination)
+        {
+            TrackingCode = trackingCode;
+            Description = description;
+            Weight = weight;
+            DeliveryFee = deliveryFee;
+            Destination = destination;
+        }
+
+        public virtual decimal EstimatedCost
+        {
+            get
+            {
+                return DeliveryFee + (Weight * 5);
+            }
+        }
+
+        public void UpdateWeight(decimal newWeight)
+        {
+            if (newWeight > 0)
+            {
+                Weight = newWeight;
+            }
+        }
+
+        public void UpdateWeight(
+            decimal newWeight,
+            decimal extraPackingWeight)
+        {
+            if (newWeight > 0 && extraPackingWeight >= 0)
+            {
+                Weight = newWeight + extraPackingWeight;
+            }
+        }
+
+        public virtual void PrintShipment()
+        {
+            Console.WriteLine($"Tracking Code : {TrackingCode}");
+            Console.WriteLine($"Description : {Description}");
+            Console.WriteLine($"Weight : {Weight} KG");
+            Console.WriteLine($"Delivery Fee : {DeliveryFee} EGP");
+            Console.WriteLine($"Estimated Cost: {EstimatedCost} EGP");
+        }
+    }
+}
