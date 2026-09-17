4c14743145788849a4bbdc4606af49e8166e1f63
Bassam Ibrahim Abu El-Saud <bassamb672005@gmail.com>
2026-09-18 01:18:17 +0300

    Add project files.

---
+++ ThirdOOPAssignment03/Program.cs
@@ -0,0 +1,22 @@
+﻿class program {
+
+    public static void Main(string[] args)
+    {
+
+        #region theoritical Questions
+        /*
+        First Overloading VS Overriding
+         Overloading having multi methods with the same name but different parameters in the same class   
+         BUT Overriding is new implementation of a method in the derived class that is already defined in the base class.
+
+        Second Question Sealed Classes and Methods
+          a) The sealed keyword prevents a class from being inherited by another class or override a method .
+          b) The sealed keyword prevents a method from being overridden in a derived class and for the class it will not be inherited.
+          C) NO, sealed Methods cannot be overridden the current implementation is the final one.
+        */
+        #endregion
+
+
+
+    }
+}
