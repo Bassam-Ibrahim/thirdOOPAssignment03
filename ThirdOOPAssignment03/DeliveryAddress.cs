1ed68db644335247dbea6d8ca9509818e13bb1b5
Bassam Ibrahim Abu El-Saud <bassamb672005@gmail.com>
2026-09-18 01:20:33 +0300

    DeliveryAddress class

---
+++ ThirdOOPAssignment03/DeliveryAddress.cs
@@ -0,0 +1,24 @@
+﻿namespace ThirdOOPAssignment
+{
+    public struct DeliveryAddress
+    {
+        private string city;
+        private string street;
+        private int buildingNumber;
+
+        public DeliveryAddress(
+            string city,
+            string street,
+            int buildingNumber)
+        {
+            this.city = city;
+            this.street = street;
+            this.buildingNumber = buildingNumber;
+        }
+
+        public string GetFullAddress()
+        {
+            return $"{city}, {street}, {buildingNumber}";
+        }
+    }
+}
